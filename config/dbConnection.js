ef95d3046c649412cc9956a376f2233fec13524c
ajaysheokand-as <ajaysheokand.as@gmail.com>
2024-08-11 12:22:49 +0530

    bug fix

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,16 +2,8 @@ const mongoose = require("mongoose");
 
 const conn = async () => {
   try {
-    console.log('MONGODB_URI:', process.env.MONGODB_URI);
     const connect = await mongoose.connect(process.env.MONGODB_URI)
-    console.log("DB Connected",
-      connect.connection.host,
-    );
-
-      // const connect = await mongoose.connect(
-      //   "mongodb://127.0.0.1:27017/CloudBilling"
-      // );
-      // console.log("DB Connected", connect.connection.host);
+    console.log("DB Connected",connect.connection.host);
        
   } catch (err) {
     console.error("DB connection error:", err);
